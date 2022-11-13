a6496f6891a3d817dcc7dc3667f30bc4b6c11e00
Joel Huggett <joel.huggett@me.com>
2022-11-13 14:57:05 -0400

    style: overview stylings

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -28,47 +28,47 @@ const DashboardPage: NextPageWithLayout = () => {
 
 	return (
 		<div className="mt-12 flex flex-wrap items-center justify-center gap-8">
-			<div className='max-w-2xl w-full px-4'>
-				<Card>
-					<div className='flex flex-col divide-y-2 divide-gray-300 gap-4'>
+			<div className='w-full px-4 justify-center flex flex-wrap gap-4'>
 					{ account.summary.filter(animal => animal.events.length > 0).map(animal => {
 						return (
-							<div className='pt-4'>
-								<p className='text-center font-bold font-serif text-xl pt-4 pb-6'>
-								<Link href={`/animals/${animal.id}`}>
-									{animal.name}
-								</Link>
-								</p>
-								<div className='w-full flex flex-wrap gap-2 justify-evenly items-center'>
-									{animal.events.sort((a, b) => a.type.name > b.type.name ? 1 : -1).map(event => {
+							<div>
+								<Card>
+									<div className=''>
+										<p className='text-center font-bold font-serif text-2xl pb-4'>
+										<Link href={`/animals/${animal.id}`}>
+											{animal.name}
+										</Link>
+										</p>
+										<div className='w-full flex flex-wrap gap-2 justify-evenly items-center'>
+											{animal.events.sort((a, b) => a.type.name > b.type.name ? 1 : -1).map(event => {
 
-										const latestEvent = event
-										const eventType = event.type
+												const latestEvent = event
+												const eventType = event.type
 
-										return (
-											<div className='px-4'>
-												<p className='text-sm'>
-												{ getEventTypeForName(eventType.name)?.displayName }
-												</p>
-												{ latestEvent && (
-													<>
-													<p className='italic text-xl'>{ formatDistanceToNow(latestEvent.createdAt) } ago</p>
-													<p className='font-thin text-sm text-right'>{ latestEvent.user.name }</p>
-													</>
-												)  }
-												{ !latestEvent && (
-													<p>Has not happened yet.</p>
-												)} 
-												
-											</div>
-										)
-									})}
-								</div>
+												return (
+													<div className='px-4'>
+														<p className='text-sm'>
+														{ getEventTypeForName(eventType.name)?.displayName }
+														</p>
+														{ latestEvent && (
+															<>
+															<p className='italic text-xl text-center py-1 font-bold'>{ formatDistanceToNow(latestEvent.createdAt) } ago</p>
+															<p className='font-thin text-sm text-right'>{ latestEvent.user.name }</p>
+															</>
+														)  }
+														{ !latestEvent && (
+															<p>Has not happened yet.</p>
+														)} 
+														
+													</div>
+												)
+											})}
+										</div>
+									</div>
+								</Card>
 							</div>
 						)
 					}) }
-					</div>
-				</Card>
 			</div>
 		</div>
   );
